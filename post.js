f510ab7a1edb3bf821ec33186c59b1c6393d413a
D Kevin <kevindemaria@gmail.com>
2021-07-20 16:50:06 +0530

    add promise

--- post.js
+++ post.js
@@ -2,48 +2,63 @@ const express = require('express');
 const mysql = require('mysql');
 
 
-const app = express();
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'kvnmaria@12',
+    database: 'leave_application'
+})
 
-app.use(express.json());
 
-app.post('/leave', (req, res) => {
-
-    const con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'kvnmaria@12',
-        database: 'leave_application'
+function connectDatabase() {
+    return new Promise((resolve, reject) => {
+        con.connect((err) => {
+            if (!err) {
+                resolve(true)
+            } else {
+                reject(err)
+            }
+        });
     })
+}
 
-    let employeeId = req.body.employeeId;
-    let fromDate = req.body.fromDate;
-    let toDate = req.body.toDate;
+const init = async () => {
 
-    if (!employeeId || !fromDate || !toDate) {
-        return res.status(400).send('Please Fill all the Details')
-    }
+    const app = express();
 
-    con.connect((err) => {
-        if (err) throw err;
+    app.use(express.json());
 
-        console.log('connected');
+    await connectDatabase();
 
+    app.post('/leave', (req, res) => {
+
+        let employeeId = req.body.employeeId;
+        let fromDate = req.body.fromDate;
+        let toDate = req.body.toDate;
+
+        if (!employeeId || !fromDate || !toDate) {
+            return res.status(400).send('Please Fill all the Details')
+        }
         const sqlQuery = `UPDATE employee 
                           SET status = 'Approved' 
                           WHERE ID = '43232' `;
 
+
         if (req.body.employeeId === '43232') {
 
-            con.query(sqlQuery, (err) => {
-
-                if (err) throw err;
-
-                return res.status(200).send('Your Leave Has been Approved');
+            con.query(sqlQuery, (err, rows) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(rows)
             })
 
+            return res.status(200).send('Your Leave Has been Approved');
         } else {
             return res.status(400).send('Please send a Appropriate EmployeeId');
         }
-    })
 
-}).listen(5000, () => console.log('Server started at port 5000'));
+    }).listen(5000, () => console.log("Server Started at port 5000"));
+}
+
+init();

46df51c104e7ca673346fb8130acba050fd52267
D Kevin <kevindemaria@gmail.com>
2021-08-10 09:52:35 +0530

    add cors middleware and change the response into json response

--- employeeData.js
+++ employeeData.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const cors = require('cors');
 const app = require('./app')
 
 const PORT = 7777;
@@ -28,6 +29,9 @@ function databaseConnection() {
     })
 }
 
+// adding the cors middleware
+app.use(cors());
+
 const init = async () => {
 
     try {
@@ -49,19 +53,33 @@ const init = async () => {
             const total_leave = req.body.total_leave;
 
             if (!id) {
-                return res.status(400).send('Please enter the EmployeeId');
+                return res.status(400).send({
+                    Message: 'Please enter the EmployeeId'
+                });
             } else if (!name) {
-                return res.status(400).send('Please enter the Name');
+                return res.status(400).send({
+                    Message: 'Please enter the Name'
+                });
             } else if (!mail) {
-                return res.status(400).send('Please enter the Email');
+                return res.status(400).send({
+                    Message: 'Please enter the Email'
+                });
             } else if (!mobile) {
-                return res.status(400).send('Please enter the Mobile No');
+                return res.status(400).send({
+                    Message: 'Please enter the Mobile No'
+                });
             } else if (!role) {
-                return res.status(400).send('Please enter the Role ');
+                return res.status(400).send({
+                    Message: 'Please enter the Role '
+                });
             } else if (!password) {
-                return res.status(400).send('Please enter the Password');
+                return res.status(400).send({
+                    Message: 'Please enter the Password'
+                });
             } else if (!total_leave) {
-                return res.status(400).send('Please enter the Total_Leave');
+                return res.status(400).send({
+                    Message: 'Please enter the Total_Leave'
+                });
             }
 
             const saltRounds = 10;
@@ -74,11 +92,13 @@ const init = async () => {
                 [id, name, mail, mobile, role, userPassword, total_leave]
             ]
 
-            con.query(sqlQuery, [values], (err, result) => {
+            con.query(sqlQuery, [values], (err) => {
 
-                if (err) return res.status(500).send('There is Some Database Error')
+                if (err) return console.log(error);
 
-                return res.send('Data has been entered Successfully')
+                return res.send({
+                    Message: 'Data has been entered Successfully'
+                })
             });
         });
 
@@ -91,12 +111,22 @@ const init = async () => {
             let fromDate = req.body.fromDate;
             let toDate = req.body.toDate;
 
-            if (!employeeId) {
-                return res.status(400).send('Please enter your EmployeeId')
-            } else if (!fromDate) {
-                return res.status(400).send('Please enter the From Date')
+            if (!employeeId && !fromDate && !toDate) {
+                return res.status(400).send({
+                    Message: 'Please enter your EmployeeId, fromDate and toDate'
+                })
+            } else if (!employeeId) {
+                return res.status(400).send({
+                    Message: 'Please Enter the EmployeeId'
+                })
             } else if (!toDate) {
-                return res.status(400).send('Please enter the To Date')
+                return res.status(400).send({
+                    Message: 'Please Enter the toDate'
+                })
+            } else if (!fromDate) {
+                return res.status(400).send({
+                    Message: 'Please Enter the fromDate'
+                })
             }
 
             const sqlQuery = 'INSERT INTO employeeLeave(fromDate, toDate, employeeId) VALUES ?';
@@ -131,13 +161,17 @@ const init = async () => {
 
                         con.query(sqlQuery, err => {
 
-                            if (err) return res.status(500).send('Some Database Error');
+                            if (err) return console.log(err.message);
 
-                            return res.status(200).send('Your Leave has been approved')
+                            return res.status(200).send({
+                                Message: 'Your Leave has been approved'
+                            })
                         })
                     }
                 } else {
-                    res.status(400).send(`Your EmployeeId Does not Match with Company's Record`);
+                    res.status(400).send({
+                        Message: `Your EmployeeId Does not Match with Company's Record`
+                    });
                 }
 
             })
@@ -149,8 +183,6 @@ const init = async () => {
     } catch (error) {
         console.log(error);
     }
-
-
 }
 
 init();

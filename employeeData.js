7f759daae2763b2edcf0d294e4a771c547edb6a0
D Kevin <kevindemaria@gmail.com>
2021-08-13 19:14:30 +0530

    remove init function remove the databaseConnection

--- employeeData.js
+++ employeeData.js
@@ -1,170 +1,162 @@
-const { app, databaseConnection, con } = require('./app');
+const { app } = require('./app');
 const bcrypt = require('bcrypt');
+const { con } = require('./databaseConnection');
 
 
-const init = async () => {
+// Route for adding an new Employee
+app.post('/employeeData', async (req, res) => {
 
     try {
-        await databaseConnection();
+        const id = req.body.id;
+        const name = req.body.name;
+        const mail = req.body.mail;
+        const mobile = req.body.mobile;
+        const role = req.body.role;
+        const password = req.body.password;
+        const total_leave = req.body.total_leave;
 
-        // Route for adding an new Employee
-        app.post('/employeeData', async (req, res) => {
+        if (!id) {
+            return res.status(400).send({
+                Message: 'Please enter the EmployeeId'
+            });
+        } else if (!name) {
+            return res.status(400).send({
+                Message: 'Please enter the Name'
+            });
+        } else if (!mail) {
+            return res.status(400).send({
+                Message: 'Please enter the Email'
+            });
+        } else if (!mobile) {
+            return res.status(400).send({
+                Message: 'Please enter the Mobile No'
+            });
+        } else if (!role) {
+            return res.status(400).send({
+                Message: 'Please enter the Role '
+            });
+        } else if (!password) {
+            return res.status(400).send({
+                Message: 'Please enter the Password'
+            });
+        } else if (!total_leave) {
+            return res.status(400).send({
+                Message: 'Please enter the Total_Leave'
+            });
+        }
 
-            try {
-                const id = req.body.id;
-                const name = req.body.name;
-                const mail = req.body.mail;
-                const mobile = req.body.mobile;
-                const role = req.body.role;
-                const password = req.body.password;
-                const total_leave = req.body.total_leave;
+        const saltRounds = 10;
 
-                if (!id) {
-                    return res.status(400).send({
-                        Message: 'Please enter the EmployeeId'
-                    });
-                } else if (!name) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Name'
-                    });
-                } else if (!mail) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Email'
-                    });
-                } else if (!mobile) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Mobile No'
-                    });
-                } else if (!role) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Role '
-                    });
-                } else if (!password) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Password'
-                    });
-                } else if (!total_leave) {
-                    return res.status(400).send({
-                        Message: 'Please enter the Total_Leave'
-                    });
-                }
+        const userPassword = await bcrypt.hash(password, saltRounds);
 
-                const saltRounds = 10;
+        const sqlQuery = 'INSERT INTO employee(ID, Name, Mail, Mobile, Role, Password, Total_Leave) VALUES ?';
 
-                const userPassword = await bcrypt.hash(password, saltRounds);
+        const values = [
+            [id, name, mail, mobile, role, userPassword, total_leave]
+        ]
 
-                const sqlQuery = 'INSERT INTO employee(ID, Name, Mail, Mobile, Role, Password, Total_Leave) VALUES ?';
+        con.query(sqlQuery, [values], (err) => {
 
-                const values = [
-                    [id, name, mail, mobile, role, userPassword, total_leave]
-                ]
+            if (err) return res.status(500).send({
+                Message: 'DataBase Error'
+            })
 
-                con.query(sqlQuery, [values], (err) => {
-
-                    if (err) return res.status(500).send({
-                        Message: 'DataBase Error'
-                    })
-
-                    return res.send({
-                        Message: 'Data has been entered Successfully'
-                    })
-                });
-            } catch (error) {
-                return res.status(500).send({
-                    Message: 'Some Server Side Error'
-                })
-            }
+            return res.send({
+                Message: 'Data has been entered Successfully'
+            })
         });
+    } catch (error) {
+        return res.status(500).send({
+            Message: 'Some Server Side Error'
+        })
+    }
+});
 
 
 
-        // Route for LeaveApplications(/leaveapplication)
-        app.post('/leaveapplication', (req, res) => {
+// Route for LeaveApplications(/leaveapplication)
+app.post('/leaveapplication', (req, res) => {
 
-            try {
-                let employeeId = req.body.employeeId;
-                let fromDate = req.body.fromDate;
-                let toDate = req.body.toDate;
+    try {
+        let employeeId = req.body.employeeId;
+        let fromDate = req.body.fromDate;
+        let toDate = req.body.toDate;
 
-                if (!employeeId && !fromDate && !toDate) {
-                    return res.status(400).send({
-                        Message: 'Please enter your EmployeeId, fromDate and toDate'
-                    })
-                } else if (!employeeId) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the EmployeeId'
-                    })
-                } else if (!toDate) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the toDate'
-                    })
-                } else if (!fromDate) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the fromDate'
-                    })
-                }
+        if (!employeeId && !fromDate && !toDate) {
+            return res.status(400).send({
+                Message: 'Please enter your EmployeeId, fromDate and toDate'
+            })
+        } else if (!employeeId) {
+            return res.status(400).send({
+                Message: 'Please Enter the EmployeeId'
+            })
+        } else if (!toDate) {
+            return res.status(400).send({
+                Message: 'Please Enter the toDate'
+            })
+        } else if (!fromDate) {
+            return res.status(400).send({
+                Message: 'Please Enter the fromDate'
+            })
+        }
 
-                const sqlQuery = 'INSERT INTO employeeLeave(fromDate, toDate, employeeId) VALUES ?';
+        const sqlQuery = 'INSERT INTO employeeLeave(fromDate, toDate, employeeId) VALUES ?';
 
-                const values = [
-                    [fromDate, toDate, employeeId]
-                ]
+        const values = [
+            [fromDate, toDate, employeeId]
+        ]
 
-                con.query(sqlQuery, [values], (err) => {
+        con.query(sqlQuery, [values], (err) => {
 
-                    if (err) return res.status(500).send({
-                        Message: 'Database Error'
-                    })
+            if (err) return res.status(500).send({
+                Message: 'Database Error'
+            })
 
-                    console.log('Data Entered Successfully');
-
-                })
-
-                const sqlQueryEmployeeId = `SELECT * FROM employee WHERE ID = '${employeeId}' `;
-
-                con.query(sqlQueryEmployeeId, (err, dbResult) => {
-
-                    if (err) return console.log(err.message)
-
-                    console.log(dbResult, req.body.employeeId);
-
-                    if (dbResult.length > 0) {
-
-                        if (req.body.employeeId == dbResult[0].ID) {
-
-                            const sqlQuery = `UPDATE employeeLeave
-                                              SET status = 'Approved'
-                                              WHERE employeeId = '${req.body.employeeId}'`;
-
-                            con.query(sqlQuery, err => {
-
-                                if (err) return res.status(500).send({
-                                    Message: 'Database Error'
-                                })
-
-                                return res.status(200).send({
-                                    Message: 'Your Leave has been approved'
-                                })
-                            })
-                        }
-                    } else {
-                        res.status(400).send({
-                            Message: `Your EmployeeId Does not Match with Company's Record`
-                        });
-                    }
-
-                })
-            } catch (error) {
-                return res.status(500).send({
-                    Message: "Some Server Side Error"
-                })
-            }
+            console.log('Data Entered Successfully');
 
         })
 
+        const sqlQueryEmployeeId = `SELECT * FROM employee WHERE ID = '${employeeId}' `;
+
+        con.query(sqlQueryEmployeeId, (err, dbResult) => {
+
+            if (err) return console.log(err.message)
+
+            console.log(dbResult, req.body.employeeId);
+
+            if (dbResult.length > 0) {
+
+                if (req.body.employeeId == dbResult[0].ID) {
+
+                    const sqlQuery = `UPDATE employeeLeave
+                                              SET status = 'Approved'
+                                              WHERE employeeId = '${req.body.employeeId}'`;
+
+                    con.query(sqlQuery, err => {
+
+                        if (err) return res.status(500).send({
+                            Message: 'Database Error'
+                        })
+
+                        return res.status(200).send({
+                            Message: 'Your Leave has been approved'
+                        })
+                    })
+                }
+            } else {
+                res.status(400).send({
+                    Message: `Your EmployeeId Does not Match with Company's Record`
+                });
+            }
+
+        })
     } catch (error) {
-        throw error
+        return res.status(500).send({
+            Message: "Some Server Side Error"
+        })
     }
-}
+
+})
+
 
 init();

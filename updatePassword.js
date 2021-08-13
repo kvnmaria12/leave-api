cfc867db04ef7d275771684b04e3fac86dbe7d67
D Kevin <kevindemaria@gmail.com>
2021-08-13 19:24:23 +0530

    remove the init function and the databaseConnection

--- updatePassword.js
+++ updatePassword.js
@@ -1,64 +1,53 @@
 const bcrypt = require('bcrypt');
-const { app, databaseConnection, con } = require('./app');
+const { app } = require('./app');
+const { con } = require('./databaseConnection');
 
-
-const init = async () => {
+// /login
+app.post('/updatePassword', async (req, res) => {
 
     try {
+        const employeeId = req.body.employeeId;
+        const password = req.body.password;
 
-        await databaseConnection();
+        if (!employeeId && !password) {
+            return res.status(400).send({
+                Message: 'Please Enter the EmployeeId and Password'
+            })
+        } else if (!employeeId) {
+            return res.status(400).send({
+                Message: 'Please Enter the EmployeeId'
+            })
+        } else if (!password) {
+            return res.status(400).send({
+                Message: 'Please Enter the Password'
+            })
+        }
 
-        // /login
-        app.post('/updatePassword', async (req, res) => {
+        const saltRounds = 10;
 
-            try {
-                const employeeId = req.body.employeeId;
-                const password = req.body.password;
+        const updatedPassword = await bcrypt.hash(password, saltRounds);
 
-                if (!employeeId && !password) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the EmployeeId and Password'
-                    })
-                } else if (!employeeId) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the EmployeeId'
-                    })
-                } else if (!password) {
-                    return res.status(400).send({
-                        Message: 'Please Enter the Password'
-                    })
-                }
-
-                const saltRounds = 10;
-
-                const updatedPassword = await bcrypt.hash(password, saltRounds);
-
-                const sqlQuery = `UPDATE employee
+        const sqlQuery = `UPDATE employee
                                   SET password = '${updatedPassword}'
                                   WHERE id = '${employeeId}'`;
 
 
-                con.query(sqlQuery, (err) => {
+        con.query(sqlQuery, (err) => {
 
-                    if (err) return res.status(500).send({
-                        Message: 'Some DataBase Error'
-                    })
+            if (err) return res.status(500).send({
+                Message: 'Some DataBase Error'
+            })
 
-                    return res.status(200).send({
-                        Message: 'Password Update Successfully'
-                    })
-                })
-            } catch (error) {
-                return res.status(500).send({
-                    Message: 'Some Server Error'
-                })
-            }
+            return res.status(200).send({
+                Message: 'Password Update Successfully'
+            })
         })
-
     } catch (error) {
-        throw error;
+        return res.status(500).send({
+            Message: 'Some Server Error'
+        })
     }
-}
+})
 
-// calling the init function
-init();
+
+
